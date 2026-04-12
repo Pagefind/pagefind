309f57a357f2dc1d3cb4f9f5f3fee3de974cdc21
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2026-04-12 23:20:48 +1200

    Use jemalloc on musl (#1120)
    * Use jemalloc on musl
    
    * Remove unused feature

--- pagefind/src/main.rs
+++ pagefind/src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(all(target_env = "musl", target_pointer_width = "64"))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 use pagefind::runner::run_indexer;
 
 #[tokio::main]
