4d230ea58025268b2cd40b096ce5d67448f8d173
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2026-09-13 21:49:49 +1200

    Align the excerpt tokenizer with the Rust indexer on U+FEFF (#1318)

--- pagefind_web_js/tests/excerpt.test.ts
+++ pagefind_web_js/tests/excerpt.test.ts
@@ -74,6 +74,12 @@ test('build narrowcapped excerpt', t => {
 	t.is(excerpt.excerpt, "vehicula <mark>ut</mark> id elit.");
 });
 
+test('build excerpt from content containing a byte order mark', t => {
+    const excerpt = build_excerpt("\uFEFFDonec id elit non mi porta gravida at eget metus.", 1, 5, [2]);
+
+	t.is(excerpt.excerpt, "id <mark>elit</mark> non mi porta");
+});
+
 test('build excerpt with ellipsis', t => {
     const excerpt = build_excerpt("Nullam id dolor id nibh ultricies vehicula ut id elit. Donec sed odio dui.", 6, 6, [7], undefined, undefined, "…");
 
